0da3577f26fd256472022644dd5bb0c4d731cc6e
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-29 21:29:44 -0300

    Add list ong routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,12 @@ const crypto = require('crypto');
 const connection = require('./database/connection');
 const routes = express.Router();
 
+routes.get('/ongs', async (request, response) => {
+  const ongs = await connection('ongs').select('*');
+  console.log(request)
+  return response.json(ongs);
+})
+
 
 routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body;

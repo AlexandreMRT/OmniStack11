2d5cdf6de5b15c20a84472f462d38bf763e42fba
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-29 21:51:42 -0300

    Add OngController. create index and create actions

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,30 +1,14 @@
 const express = require('express');
-const crypto = require('crypto');
+
+const OngController = require('./controllers/OngsController');
+
 const connection = require('./database/connection');
+
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*');
-  console.log(request)
-  return response.json(ongs);
-})
+routes.get('/ongs', OngController.index);
 
 
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-
-  return response.json({ id });
-});
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;

068ac24f898c81a0639fd53a092c81c6ac63d8e1
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-31 12:00:46 -0300

    Add logon page styles

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FiLogIn } from 'react-icons/fi';
+
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
@@ -7,14 +9,15 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
   return(
-      <div className="logon-conteiner">
+      <div className="logon-container">
         <section className="form">
         <img src={logoImg} alt="Be the hero logo" />
         <form>
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID"/>
-          <button type="submit">Entrar</button>
+          <button className="button" type="submit">Entrar</button>
           <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
             </a>
         </form>

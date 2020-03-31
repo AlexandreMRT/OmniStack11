105654fba61187d3ff654cb212e09df236461e7d
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-31 12:11:36 -0300

    Add routes to the project

---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import React from 'react';
+
+import Logon from './pages/Logon'
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Logon} />
+      </Switch>
+    </BrowserRouter>
+  )
+}

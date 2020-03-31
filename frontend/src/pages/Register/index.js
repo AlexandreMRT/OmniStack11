acf38eb245b5c9335254cad54ae4c156d9723d59
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-31 12:38:37 -0300

    Register page with styles

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css'
+
+import { Link } from 'react-router-dom';
+
+import { FiArrowLeft } from 'react-icons/fi';
+
+
+
+export default function Register() {
+  return(
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be the hero" />
+
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+            </Link>
+        </section>
+
+        <form>
+          <input placeholder="Nome da ONG" />
+          <input placeholder="E-Mail" type="email"/>
+          <input placeholder="WhatsApp" />
+          
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="UF" style={{ width: 80}}/>
+          </div>
+
+          <button className="button" type="submit" >Cadastrar</button>
+
+
+        </form>
+      </div>
+    </div>
+  );
+}
